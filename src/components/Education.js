e4782459b0daab2953cb8bc88d2e14ccd721ca37
simionatoambrosio <gabriel252004@outlook.com>
2022-03-23 12:54:36 -0300

    main

--- src/components/Education.js
+++ src/components/Education.js
@@ -7,6 +7,8 @@ export default function Education() {
         <div className="Education" id="education">
             <h1><span>Formação Acadêmica</span></h1>
             <p>Algum texto q eu vo escrever e etc</p>
+            <h2>Ensino Superior</h2>
+            <p>Cursando Análise e Desenvolvimento de Sistemas na FATEC</p>
         </div>
     )
 }
