bc88df85462e34c20a945c2dfdd3d0e7b24295f4
simionatoambrosio <gabriel252004@outlook.com>
2022-04-14 09:02:55 -0300

    add: profile picture using github api

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import Picture from './images/imagem-vermelho.jpeg'
+// import Picture from './images/imagem-vermelho.jpeg'
 import './Profile.css'
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import Aos from "aos";
 import "aos/dist/aos.css"
@@ -16,9 +16,21 @@ export default function Profile() {
             once: true
          })
       }, []);
+
+    const [profilePic, setProfilePic] = useState({})
+    
+    useEffect(() =>{
+        fetch('https://api.github.com/users/simionatoambrosio')
+        .then(response => response.json())
+        .then(data => setProfilePic(data))
+        console.log(profilePic)
+      }, [])
+
+
+
     return(
         <div className="Profile"  >
-            <img src={Picture} id="my-picture" alt="fotografia minha"></img>
+            <img src={profilePic.avatar_url} id="my-picture" alt="fotografia minha"></img>
             <h1 data-aos="fade-right">Olá, meu nome é Gabriel &#128075;</h1>
             <p id="description" data-aos="fade-right">Atualmente estou estudando com foco em <span>Front-End</span> e cursando <span>Análise e Desenvolvimento de Sistemas </span>na Faculdade de Tecnologia de São Paulo</p>
         </div>
