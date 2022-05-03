a5ad7f3cb2b78b29f932bdc0a0ad39f4cd67880c
simionatoambrosio <gabriel252004@outlook.com>
2022-05-02 22:14:21 -0300

    add: switch theme (light and dark)

--- src/App.js
+++ src/App.js
@@ -7,20 +7,35 @@ import AboutMe from './components/AboutMe';
 import Education from './components/Education';
 import Projects from './components/Projects';
 
-import { useEffect } from 'react';
+import { useEffect, useState, createContext } from 'react';
 
 import Aos from "aos";
 import "aos/dist/aos.css"
+import ReactSwitch from 'react-switch';
+
+export const ThemeContext = createContext(null)
 
 function App() {
+  const [theme, setTheme] = useState("light")
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"))
+  }
+
   return (
-    <div className="App">
-      <Profile data-aos="fade-up" />
-      <MenuOptions data-aos="fade-up"/>
-      <AboutMe data-aos="fade-up"/>
-      <Education data-aos="fade-up"/>
-      <Projects data-aos="fade-up"/>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme} >
+        <div className='switch'>
+          {/* <p>{theme === "light" ? "Light Mode" : "Dark Mode"}</p> */}
+          <p>Dark Mode</p>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
+        <Profile data-aos="fade-up" />
+        <MenuOptions data-aos="fade-up" />
+        <AboutMe data-aos="fade-up" />
+        <Education data-aos="fade-up" />
+        <Projects data-aos="fade-up" />
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
